2034d6b0913467aea0ba00aee36f8837f1aabacc
Bartosz Król <barto.kro@gmail.com>
2021-09-21 00:13:13 +0200

    import useState and useEffect and create fetchApi function. Create CharactersTable.js

--- src/component/App.js
+++ src/component/App.js
@@ -1,10 +1,25 @@
+import { useState, useEffect } from "react";
 import FilteringSection from "../layouts/FilteringSection";
 import ButtonsSection from "../layouts/ButtonsSection";
+import CharactersTable from "../layouts/CharactersTable";
 import "../styles/main.css";
 
-const basicClassName = "starWarsApi";
-
 function App() {
+	const basicClassName = "starWarsApi";
+
+	const api = `https://swapi.dev/api/people`;
+	const [characters, setCharactes] = useState([]);
+
+	useEffect(() => {
+		const fetchApi = () => {
+			fetch(api)
+				.then((data) => data.json())
+				.then((data) => setCharactes([...data.results]))
+				.catch((error) => console.log(error));
+		};
+
+		fetchApi();
+	}, []);
 	return (
 		<div className={basicClassName}>
 			<h1 className={`${basicClassName}__heading`}>Characters</h1>
@@ -16,6 +31,7 @@ function App() {
 					basicClassName={`${basicClassName}__inputsAndBtnsSection`}
 				/>
 			</div>
+			<CharactersTable />
 		</div>
 	);
 }
---
+++ src/layouts/CharactersTable.js
@@ -0,0 +1,5 @@
+const CharactersTable = () => {
+	return <div>Characters Table</div>;
+};
+
+export default CharactersTable;
